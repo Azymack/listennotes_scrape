e739acf2033fe0d7fa3f7d6068ae46ab2179bf4f
Azymack <orillath@outlook.com>
2024-09-23 11:20:03 +0200

    initial result

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 import os
 
 import constants
+import utils
 
 os.system("taskkill /f /im chrome.exe")
 
@@ -13,9 +15,6 @@ options.add_argument("--log-level=3")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--user-data-dir=C:\\Users\\ff\\AppData\\Local\\Google\\Chrome\\User Data")
 options.add_argument('--profile-directory=Profile 13')
-
-# Remove this if not needed
-# options.add_argument('--remote-debugging-pipe')
 
 # Create a new Chrome session
 driver = webdriver.Chrome(options=options)
@@ -32,7 +31,12 @@ try:
 
                 # Find podcast cards (10 cards per page)
                 cards = driver.find_elements(By.CLASS_NAME, "ln-page-card")
-                print(cards)
+                for card_div in cards:
+                    card_info = utils.get_initial_info(card_div)
+                    if card_info is not None:
+                        utils.write_initial_info_into_json(region_code, card_info)
+                    else:
+                        pass
 
             except Exception as e:
                 print(f'Error loading page {i+1} for region {region_code}: {e}')
---
+++ utils.py
@@ -0,0 +1,49 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import json
+import os
+
+def get_initial_info(card_div):
+    try:
+        # Locate the header div and title in one go
+        title_div = card_div.find_element(By.CSS_SELECTOR, ".flex.items-center h2")
+        tag_div_arr = card_div.find_elements(By.CSS_SELECTOR, ".flex.items-center .flex-none div")
+        by_div = card_div.find_element(By.CSS_SELECTOR, "a.ml-1.text-black.inline")
+        print(by_div, "------------------------------------")
+        # Get the title text and URL
+        title_text = title_div.text
+        title_url = title_div.find_element(By.TAG_NAME, "a").get_attribute("href")
+        tag_1 = tag_div_arr[0].text
+        tag_2 = tag_div_arr[2].text
+        by = by_div.text
+        
+        return {"title": title_text, "url": title_url, "tag_1": tag_1,  "tag_2": tag_2, "by": by}
+    except (IndexError, NoSuchElementException):
+        return None
+
+def write_initial_info_into_json(region, info):
+    json_file_path = "result/{}.json".format(region)
+    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+    # Check if the file exists
+    if os.path.exists(json_file_path):
+        # Read existing data
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+    else:
+        # If the file does not exist, start with an empty list
+        data = []
+
+    # Append the new data
+    data.append({
+        "title": info["title"],
+        "url": info["url"],
+        "LS": info["tag_1"],
+        "rank": info["tag_2"],
+        "By": info["by"],
+        "email": "",
+        "hosts": ""
+    })
+
+    # Write updated data back to the JSON file
+    with open(json_file_path, 'w') as file:
+        json.dump(data, file, indent=2)
